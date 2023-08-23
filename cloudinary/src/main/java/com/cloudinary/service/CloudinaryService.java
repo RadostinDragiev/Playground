285bbae778e37041774ced611b691e929ccf4116
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2023-08-23 22:47:41 +0300

    Delete file

--- cloudinary/src/main/java/com/cloudinary/service/CloudinaryService.java
+++ cloudinary/src/main/java/com/cloudinary/service/CloudinaryService.java
@@ -1,12 +1,14 @@
 package com.cloudinary.service;
 
 import com.cloudinary.*;
+import com.cloudinary.api.ApiResponse;
 import com.cloudinary.utils.ObjectUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
 
 @Slf4j
@@ -20,29 +22,30 @@ public class CloudinaryService {
         this.cloudinary = cloudinary;
     }
 
-    /**
-     * ObjectUtils.asMap
-     * <p>
-     * Define file type
-     * - resource_type -> video /auto/ raw
-     * <p>
-     * Use the uploaded file name
-     * - use_filename -> true / false
-     * <p>
-     * Cloudinary will add random suffix after the file name to make it unique
-     * - unique_filename -> true / false
-     * <p>
-     * Put custom name to the file
-     * - public_id -> (custom)
-     * <p>
-     * Create folder
-     * - folder -> "pets/my_dog"
-     */
     public void uploadFile() throws IOException {
         Uploader uploader = this.cloudinary.uploader();
 
         Map upload = uploader.upload("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Olympic_rings_without_rims.svg/2880px-Olympic_rings_without_rims.svg.png", ObjectUtils.emptyMap());
 
         log.info("File uploaded to URL -> " + upload.get("secure_url"));
+        log.info("File public id -> " + upload.get("public_id"));
+    }
+
+    public void fetchFile() throws Exception {
+        Api api = this.cloudinary.api();
+
+        api.resources(ObjectUtils.asMap("", ""));
+    }
+
+    public void deleteFile(String[] publicIds) throws Exception {
+//        Uploader uploader = this.cloudinary.uploader();
+//        uploader.destroy("publicId", ObjectUtils.asMap("invalidate", true));
+
+        // Admin API
+        Api api = this.cloudinary.api();
+        ApiResponse invalidate = api.deleteResources(Arrays.asList(publicIds), ObjectUtils.asMap("invalidate", true));
+        Map<String, String> map = (Map<String, String>) invalidate.get("deleted");
+        map.forEach((key, value) -> log.warn(String.format("For public id: %s response is %s", key, value)));
+        System.out.println();
     }
 }
--- cloudinary/src/main/java/com/cloudinary/web/UserController.java
+++ cloudinary/src/main/java/com/cloudinary/web/UserController.java
@@ -3,7 +3,9 @@ package com.cloudinary.web;
 import com.cloudinary.service.CloudinaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -20,6 +22,12 @@ public class UserController {
     @PostMapping("/upload")
     public ResponseEntity<Void> uploadProfilePicture() throws IOException {
         this.cloudinaryService.uploadFile();
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete")
+    private ResponseEntity<Void> deleteFile(@RequestParam(name = "publicId") String publicId) throws Exception {
+        this.cloudinaryService.deleteFile(new String[] {"tb8da82ao9nqjualdjtj", "olympic_flag2"});
         return ResponseEntity.ok().build();
     }
 }
